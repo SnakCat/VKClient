f763071bda704a855fc652de40bb43355dcbf890
SnakCat <snakkdss@gmail.com>
2024-11-14 11:32:37 +0300

    initial commit

---
+++ VKClient/VKClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKClientApp.swift
+//  VKClient
+//
+//  Created by Дмитрий Трушин on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct VKClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
